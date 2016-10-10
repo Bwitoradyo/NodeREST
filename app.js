298a24ad925fc4464ba02147e4e08e6edc061fa3
Bambang Witoradyo <bambang.witoradyo@sonoco.com>
2016-10-10 17:22:21 -0400

    implement single search by Id

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ var bookRouter = express.Router();
 
 bookRouter.route('/Books')
     .get(function (req, res) {
-       Book.find(function (err, books) {
+        var query={};
+        if (req.query.genre){
+            query.genre = req.query.genre;
+        }
+        Book.find(query, function (err, books) {
           if(err)
               res.status(500).send(err)
           else
@@ -19,6 +23,15 @@ bookRouter.route('/Books')
        });
     });
 
+bookRouter.route('/Books/:bookId')
+    .get(function (req, res) {
+        Book.findById(req.params.bookId, function (err, book) {
+            if(err)
+                res.status(500).send(err)
+            else
+                res.json(book);
+        });
+    });
 app.use('/api', bookRouter);
 
 app.get('/', function (req, res) {
